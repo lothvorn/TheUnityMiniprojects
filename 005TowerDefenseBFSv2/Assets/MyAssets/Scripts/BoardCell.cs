8c04b5713643933ff4a53ecfabc58bd5eb93a856
Eldir <lothvorn@gmail.com>
2015-04-14 20:36:06 +0200

    Board instantiator and tower type 0
    Board instantiator v01 finished and working. Doing tower type 0

--- 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/BoardCell.cs
+++ 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/BoardCell.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BoardInstantiator : MonoBehaviour {
+public class BoardCell : MonoBehaviour {
+
+	public GameObject northN;
+	public GameObject eastN;
+	public GameObject southN;
+	public GameObject westN;
+
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
---
+++ 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/BoardGenerator.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * create and link board objects in
+ */
+
+
+public class BoardGenerator : MonoBehaviour {
+	//public
+	public int xSize=5;
+	public int ySize=5;
+
+	//references to other game objects
+	public GameObject boardHolder;
+	public GameObject cell;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 creationPointer = new Vector3 (0,0);
+
+		GameObject [,] boardMatrix = new GameObject[xSize, ySize];
+
+		//first create the cell game objects and use boardMatrix to track them
+		for (int x = 0; x < xSize; x++){
+			for (int y = 0; y < ySize; y++){
+				GameObject newCell = (GameObject) Instantiate (cell, creationPointer, Quaternion.identity);
+				newCell.transform.name = "Cell " + x + "-" + y;
+				newCell.transform.parent = boardHolder.transform;
+				newCell.GetComponentInChildren<TextMesh>().text = x + "-"+ y;
+				boardMatrix [x,y] = newCell;
+
+				creationPointer.x++;
+			}
+			creationPointer.x = 0;
+			creationPointer.z++;
+		}
+
+
+		//now use boardMatrix to link each cell to its neighbours. Each cell will kwnow who are its neighbours
+		for (int y = 0; y < ySize; y++){
+			for (int x = 0; x < ySize; x++){
+				SetNeighbours(x,y, boardMatrix);
+			}
+		}
+
+
+	}
+
+
+	private void SetNeighbours(int x, int y, GameObject [,] board){
+		if (x > 0)
+			board[y,x].GetComponent<BoardCell>().westN = board [y,x-1];
+		if (x < xSize-1)
+			board[y,x].GetComponent<BoardCell>().eastN = board [y,x+1];
+		if (y>0)
+			board [y,x].GetComponent<BoardCell>().southN = board [y-1,x];
+		if (y<ySize-1)
+			board [y,x].GetComponent<BoardCell>().northN = board [y+1,x];
+
+	}
+
+
+
+}
