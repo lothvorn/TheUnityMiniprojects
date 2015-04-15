d533c686c5009ce0972c5ca6286e8ca047a3246e
Eldir <lothvorn@gmail.com>
2015-04-15 22:41:13 +0200

    Creating enemies
    Only minor details until now

--- 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/Tower/Tower.cs
+++ 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/Tower/Tower.cs
@@ -8,8 +8,36 @@ public class Tower : MonoBehaviour {
 	public float fireDelay = 10;
 	public int damage = 5;
 	public int range = 5;
+	
+	public GameObject turretHead;
+	public GameObject cannonMuzzle;
+
+	//internal vars (to hide)
+	float fireTimer = 0;
+	public GameObject target = null;
 
 
 
+	public void Update (){
+		if (target == null){ //|| targetOutOfRange
+			FindNearestTarget();
+		}
+
+		AimTarget ();
+	}
+
+
+
+	private void FindNearestTarget(){
+		GameObject [] enemies;
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject nearest = null;
+	}
+
+	private void AimTarget(){
+		if (target != null){
+			turretHead.transform.LookAt (target.transform);
+		}
+	}
 
 }
