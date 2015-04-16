c49a27709c7ab31c4cb5f33760d5a893b6826a8c
Eldir <lothvorn@gmail.com>
2015-04-16 21:36:46 +0200

    Target selection for towers
    Now towers always look to the nearest target.

--- 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/Tower/Tower.cs
+++ 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/Tower/Tower.cs
@@ -19,19 +19,38 @@ public class Tower : MonoBehaviour {
 
 
 	public void Update (){
-		if (target == null){ //|| targetOutOfRange
+		if (target == null || DistanceToTarget() > range ){ //|| targetOutOfRange
 			FindNearestTarget();
 		}
 
 		AimTarget ();
+
+
 	}
 
 
 
+
+	private float DistanceToTarget() {
+		return (transform.position - target.transform.position).magnitude;
+	}
 	private void FindNearestTarget(){
 		GameObject [] enemies;
 		enemies = GameObject.FindGameObjectsWithTag("Enemy");
-		GameObject nearest = null;
+		GameObject closest = null;
+		float closestDistance = Mathf.Infinity;
+		Vector3 position = transform.position;
+
+		foreach (GameObject en in enemies){
+			Vector3 distanceVector = en.transform.position - transform.position;
+			float thisDistance = distanceVector.magnitude;
+			if (thisDistance < closestDistance){
+				closest = en;
+				closestDistance = thisDistance;
+			}
+		}
+
+		target = closest;
 	}
 
 	private void AimTarget(){
