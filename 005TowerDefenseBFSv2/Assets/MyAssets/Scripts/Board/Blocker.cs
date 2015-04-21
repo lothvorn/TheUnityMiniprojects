21a7157eca8beec760460f904e9ba93d7b82d7db
Eldir <lothvorn@gmail.com>
2015-04-21 22:29:36 +0200

    Blockers added

---
+++ 005TowerDefenseBFSv2/Assets/MyAssets/Scripts/Board/Blocker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blocker : BoardElement {
+
+	// Use this for initialization
+	void Start () {
+		boardCell.blocked = true;
+	}
+	
+
+}
